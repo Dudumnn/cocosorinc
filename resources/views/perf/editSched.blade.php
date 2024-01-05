b0b85a2dbdfe94096590903b8f91d6708e2001fe
Dudumnn <=>
2024-01-05 10:54:09 +0800

    vbcvvx

--- resources/views/perf/editSched.blade.php
+++ resources/views/perf/editSched.blade.php
@@ -47,7 +47,7 @@
                                     type="text" 
                                     name="shift"  
                                     value="{{ $sched->shift }}"
-                                    class="bg-gray-300 pointer-events-none block w-full border-0 py-1.5 text-gray-500 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm 
+                                    class="bg-gray-300 pointer-events-none block w-full border-0 pl-2 py-1.5 text-gray-500 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm 
                                     sm:leading-6">
                             </div>
                         </div>
