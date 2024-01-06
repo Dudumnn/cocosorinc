8f7232e762d60606278f397392973ebffb7ca368
Dudumnn <=>
2024-01-06 23:00:28 +0800

    dfsdfdsf

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,7 @@ Route::get('/output', [AuthController::class, 'output']);
 Route::post('/addoutput', [AuthController::class, 'addoutput']);
 
 Route::get('/users', [AuthController::class, 'index']);
+Route::put('/editProfile/{id}', [AuthController::class, 'editProfile']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
