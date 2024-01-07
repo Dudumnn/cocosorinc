3b818e313a291de7bd6ebd0add4dea3ca6f8c878
Dudumnn <=>
2024-01-07 22:53:17 +0800

    uydsh

--- resources/views/livewire/weekly-cal.blade.php
+++ resources/views/livewire/weekly-cal.blade.php
@@ -59,14 +59,6 @@
                     @endwhile
                     <th scope="col" class="px-4 py-3">
                         <button class="flex items-center gap-1">
-                            Count
-                            <svg xmlns="http://www.w3.org/2000/svg" width="11" height="11" fill="currentColor" class="bi bi-arrow-down-up" viewBox="0 0 16 16">
-                                <path fill-rule="evenodd" d="M11.5 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L11 2.707V14.5a.5.5 0 0 0 .5.5zm-7-14a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L4 13.293V1.5a.5.5 0 0 1 .5-.5z"/>
-                            </svg>
-                        </button>
-                    </th>
-                    <th scope="col" class="px-4 py-3">
-                        <button class="flex items-center gap-1">
                             Average
                             <svg xmlns="http://www.w3.org/2000/svg" width="11" height="11" fill="currentColor" class="bi bi-arrow-down-up" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M11.5 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L11 2.707V14.5a.5.5 0 0 0 .5.5zm-7-14a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L4 13.293V1.5a.5.5 0 0 1 .5-.5z"/>
@@ -76,6 +68,14 @@
                     <th scope="col" class="px-4 py-3">
                         <button class="flex items-center gap-1">
                             Potential
+                            <svg xmlns="http://www.w3.org/2000/svg" width="11" height="11" fill="currentColor" class="bi bi-arrow-down-up" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M11.5 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L11 2.707V14.5a.5.5 0 0 0 .5.5zm-7-14a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L4 13.293V1.5a.5.5 0 0 1 .5-.5z"/>
+                            </svg>
+                        </button>
+                    </th>
+                    <th scope="col" class="px-4 py-3">
+                        <button class="flex items-center gap-1">
+                            Count
                             <svg xmlns="http://www.w3.org/2000/svg" width="11" height="11" fill="currentColor" class="bi bi-arrow-down-up" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M11.5 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L11 2.707V14.5a.5.5 0 0 0 .5.5zm-7-14a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L4 13.293V1.5a.5.5 0 0 1 .5-.5z"/>
                             </svg>
@@ -127,23 +127,23 @@
                                 @endphp
                             </td>
                         @endwhile
-
-                        <td class="px-4 py-4">
-                            {{ $dayCount }}
-                        </td>
                         
                         @php
                             $average = ($dayCount > 0) ? number_format($sum / $dayCount, 3) : 0;
                         @endphp
-                        <td class="px-4 py-4">
+                        <td class="px-4 py-4 bg-red-200">
                             {{ $average }}
                         </td>
                         
                         @php
                             $potential = ($max > 0) ? number_format($sum / $max, 3) : 0;
                         @endphp
-                        <td class="px-4 py-4">
+                        <td class="px-4 py-4 bg-orange-200">
                             {{ $potential }}
+                        </td>
+
+                        <td class="px-4 py-4">
+                            {{ $dayCount }}
                         </td>
                     </tr>
                 @empty
