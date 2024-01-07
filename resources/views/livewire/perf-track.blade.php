d5252c17f1318b2539361fc0d682b84a74eb27f9
Dudumnn <=>
2024-01-07 16:31:31 +0800

    resxcuj

--- resources/views/livewire/perf-track.blade.php
+++ resources/views/livewire/perf-track.blade.php
@@ -30,16 +30,6 @@
                 </select>
             </div>
         </div>
-        <div class="flex">
-            <button x-data x-on:click="$dispatch('open-modal')" class="flex items-center bg-gray-500 rounded text-sm p-2.5 text-white gap-1">
-                New Schedule
-
-                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="white" class="bi bi-plus-lg" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"/>
-                </svg>
-            </button>
-            @livewire('add-schedule')
-        </div>
     </div>
     <div class="overflow-x-auto">
         <table class="w-full border border-gray-200 text-sm text-left text-gray-500">
