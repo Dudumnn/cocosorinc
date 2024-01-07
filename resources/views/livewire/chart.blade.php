47adcaba62a24ce8d68dc88bec5e9aa1bb436155
Dudumnn <=>
2024-01-08 03:45:51 +0800

    jhgsbks

--- resources/views/livewire/chart.blade.php
+++ resources/views/livewire/chart.blade.php
@@ -1,154 +1,75 @@
 
 <div class="max-w-sm w-full bg-white rounded-lg shadow dark:bg-gray-800 p-4 md:p-6">
-    <div class="flex justify-between border-gray-200 border-b dark:border-gray-700 pb-3">
-      <dl>
-        <dt class="text-base font-normal text-gray-500 dark:text-gray-400 pb-1">Profit</dt>
-        <dd class="leading-none text-3xl font-bold text-gray-900 dark:text-white">$5,405</dd>
-      </dl>
-      <div>
-        <span class="bg-green-100 text-green-800 text-xs font-medium inline-flex items-center px-2.5 py-1 rounded-md dark:bg-green-900 dark:text-green-300">
-          <svg class="w-2.5 h-2.5 me-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 14">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13V1m0 0L1 5m4-4 4 4"/>
-          </svg>
-          Profit rate 23.5%
-        </span>
-      </div>
-    </div>
-  
-    <div class="grid grid-cols-2 py-3">
-      <dl>
-        <dt class="text-base font-normal text-gray-500 dark:text-gray-400 pb-1">Income</dt>
-        <dd class="leading-none text-xl font-bold text-green-500 dark:text-green-400">$23,635</dd>
-      </dl>
-      <dl>
-        <dt class="text-base font-normal text-gray-500 dark:text-gray-400 pb-1">Expense</dt>
-        <dd class="leading-none text-xl font-bold text-red-600 dark:text-red-500">-$18,230</dd>
-      </dl>
-    </div>
-  
-    <div id="bar-chart"></div>
-      <div class="grid grid-cols-1 items-center border-gray-200 border-t dark:border-gray-700 justify-between">
-        <div class="flex justify-between items-center pt-5">
-          <!-- Button -->
-          <button
-            id="dropdownDefaultButton"
-            data-dropdown-toggle="lastDaysdropdown"
-            data-dropdown-placement="bottom"
-            class="text-sm font-medium text-gray-500 dark:text-gray-400 hover:text-gray-900 text-center inline-flex items-center dark:hover:text-white"
-            type="button">
-            Last 6 months
-            <svg class="w-2.5 m-2.5 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
-            </svg>
-          </button>
-          <a
-            href="#"
-            class="uppercase text-sm font-semibold inline-flex items-center rounded-lg text-blue-600 hover:text-blue-700 dark:hover:text-blue-500  hover:bg-gray-100 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700 px-3 py-2">
-            Revenue Report
-            <svg class="w-2.5 h-2.5 ms-1.5 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
-            </svg>
-          </a>
-        </div>
-      </div>
-  </div>
+    <!-- Radial Chart -->
+    <div class="py-6" id="radial-chart"></div>
+</div>
   
   <script>
     // ApexCharts options and config
     window.addEventListener("load", function() {
-      var options = {
-        series: [
-          {
-            name: "Income",
-            color: "#31C48D",
-            data: ["1420", "1620"],
-          }
-        ],
-        chart: {
-          sparkline: {
-            enabled: false,
-          },
-          type: "bar",
-          width: "100%",
-          height: 400,
-          toolbar: {
-            show: false,
-          }
-        },
-        fill: {
-          opacity: 1,
-        },
-        plotOptions: {
-          bar: {
-            horizontal: true,
-            columnWidth: "100%",
-            borderRadiusApplication: "end",
-            borderRadius: 6,
-            dataLabels: {
-              position: "top",
+      const getChartOptions = () => {
+          return {
+            series: [90, 85, 70],
+            colors: ["#1C64F2", "#16BDCA", "#FDBA8C"],
+            chart: {
+              height: "380px",
+              width: "100%",
+              type: "radialBar",
+              sparkline: {
+                enabled: true,
+              },
             },
-          },
-        },
-        legend: {
-          show: true,
-          position: "bottom",
-        },
-        dataLabels: {
-          enabled: false,
-        },
-        tooltip: {
-          shared: true,
-          intersect: false,
-          formatter: function (value) {
-            return "$" + value
-          }
-        },
-        xaxis: {
-          labels: {
-            show: true,
-            style: {
-              fontFamily: "Inter, sans-serif",
-              cssClass: 'text-xs font-normal fill-gray-500 dark:fill-gray-400'
+            plotOptions: {
+              radialBar: {
+                track: {
+                  background: '#E5E7EB',
+                },
+                dataLabels: {
+                  show: false,
+                },
+                hollow: {
+                  margin: 0,
+                  size: "32%",
+                }
+              },
             },
-            formatter: function(value) {
-              return "$" + value
-            }
-          },
-          categories: ["500 - 1000", "1001 - 1499",],
-          axisTicks: {
-            show: false,
-          },
-          axisBorder: {
-            show: false,
-          },
-        },
-        yaxis: {
-          labels: {
-            show: true,
-            style: {
+            grid: {
+              show: false,
+              strokeDashArray: 4,
+              padding: {
+                left: 2,
+                right: 2,
+                top: -23,
+                bottom: -20,
+              },
+            },
+            labels: ["Done", "In progress", "To do"],
+            legend: {
+              show: true,
+              position: "bottom",
               fontFamily: "Inter, sans-serif",
-              cssClass: 'text-xs font-normal fill-gray-500 dark:fill-gray-400'
+            },
+            tooltip: {
+              enabled: true,
+              x: {
+                show: false,
+              },
+            },
+            yaxis: {
+              show: false,
+              labels: {
+                formatter: function (value) {
+                  return value + '%';
+                }
+              }
             }
           }
-        },
-        grid: {
-          show: true,
-          strokeDashArray: 4,
-          padding: {
-            left: 2,
-            right: 2,
-            top: -20
-          },
-        },
-        fill: {
-          opacity: 1,
         }
-      }
-  
-      if(document.getElementById("bar-chart") && typeof ApexCharts !== 'undefined') {
-        const chart = new ApexCharts(document.getElementById("bar-chart"), options);
-        chart.render();
-      }
+        
+        if (document.getElementById("radial-chart") && typeof ApexCharts !== 'undefined') {
+          var chart = new ApexCharts(document.querySelector("#radial-chart"), getChartOptions());
+          chart.render();
+        }
     });
   </script>
   
