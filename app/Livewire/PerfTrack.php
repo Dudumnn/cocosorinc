0d0b9db2d169850e4d2102c899d00739a0a574a0
Dudumnn <=>
2024-01-07 16:20:31 +0800

    trewsdfg

--- app/Livewire/PerfTrack.php
+++ app/Livewire/PerfTrack.php
@@ -3,11 +3,28 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use Livewire\WithPagination;
+use App\Models\Sched;
 
 class PerfTrack extends Component
 {
+    use WithPagination;
+    public $search = '';
+
+    public $perPage = 5;
+
+    public $shift = '';
+    
     public function render()
     {
-        return view('livewire.perf-track');
+        return view('livewire.schedule',
+        [
+            'scheds' => Sched::search($this->search)
+            ->when($this->shift !== '',function($query){
+                $query->where('shift',$this->shift);
+            })
+            ->paginate($this->perPage)
+        ]
+        );
     }
 }
