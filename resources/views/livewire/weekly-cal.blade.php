b436d1ccc76161bbfd6791206295a29993b36b1b
Dudumnn <=>
2024-01-07 23:37:33 +0800

    ndgsjjs

--- resources/views/livewire/weekly-cal.blade.php
+++ resources/views/livewire/weekly-cal.blade.php
@@ -1,6 +1,6 @@
 <div>
     <div class="bg-white relative shadow-xl border border-gray-200 sm:rounded-sm overflow-hidden w-full px-5 py-4 mx-6 my-6 mb-5">
-        <div class="grid grid-cols-1 gap-x-3 w-full sm:grid-cols-12">
+        <div class="grid grid-cols-1 gap-x-3 w-full sm:grid-cols-12 p-4">
             <div class="sm:col-span-4 border border-gray-200 rounded-sm text-xs">
                 <div class="w-full border-b px-3 py-2 ">
                     <span class="text-xs font-medium text-gray-500">Employees</span>
