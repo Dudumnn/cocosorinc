7a4b72ce05d71b0715964a6f1ee54d080bf0582e
Dudumnn <=>
2024-01-08 06:44:05 +0800

    fcsvjs

--- resources/views/livewire/weekly-cal.blade.php
+++ resources/views/livewire/weekly-cal.blade.php
@@ -105,7 +105,7 @@
     <div class="bg-white relative shadow-xl border border-gray-200 sm:rounded-sm overflow-hidden w-full px-5 py-4 mx-6 my-6 mb-5">
         <div class="grid grid-cols-1 gap-x-3 w-full sm:grid-cols-12 p-2">
             <div class="sm:col-span-12 flex justify-center">
-                <span>{{ \Carbon\Carbon::parse($date->start_date)}}, {{\Carbon\Carbon::parse($date->end_date)}}</span>
+                <span>{{ \Carbon\Carbon::parse($date->start_date)->format('Y-m-d')}}, {{\Carbon\Carbon::parse($date->end_date)->format('Y-m-d')}}</span>
                 <span>{{$date->time_in}}, {{$date->time_out}}</span>
             </div>
             <div class="sm:col-span-4 border border-gray-200 rounded-sm text-xs">
