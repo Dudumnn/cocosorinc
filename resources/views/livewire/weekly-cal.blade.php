21947c273222bfcbd9a4a9687ca1436dd83b4ab6
Dudumnn <=>
2024-01-08 06:55:51 +0800

    hfcvs

--- resources/views/livewire/weekly-cal.blade.php
+++ resources/views/livewire/weekly-cal.blade.php
@@ -104,10 +104,10 @@
     </div>
     <div class="bg-white relative shadow-xl border border-gray-200 sm:rounded-sm overflow-hidden w-full px-5 py-4 mx-6 my-6 mb-5">
         <div class="grid grid-cols-1 gap-x-3 w-full sm:grid-cols-12 p-2">
-            <div class="sm:col-span-12 sm:flex sm:flex-col sm:gap-y-1 sm:items-center text-center text-lg font-bold">
+            <div class="sm:col-span-12 sm:flex sm:flex-col sm:items-center text-center text-lg font-bold">
                 <span>{{$emp->shift}} Shift Sheller Below Quota</span>
-                <span>{{ \Carbon\Carbon::parse($date->start_date)->format('M d, Y')}}, {{\Carbon\Carbon::parse($date->end_date)->format('M d, Y')}}</span>
-                <span>{{$date->time_in}}, {{$date->time_out}}</span>
+                <span class="text-base">{{ \Carbon\Carbon::parse($date->start_date)->format('M d, Y')}}, {{\Carbon\Carbon::parse($date->end_date)->format('M d, Y')}}</span>
+                <span class="text-base">{{$date->time_in}}, {{$date->time_out}}</span>
             </div>
             <div class="sm:col-span-4 border border-gray-200 rounded-sm text-xs">
                 <div class="w-full">
