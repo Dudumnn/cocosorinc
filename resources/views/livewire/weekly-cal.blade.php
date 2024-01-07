0622bddac7ba7e1ad967b5f2c987e5198290c376
Dudumnn <=>
2024-01-07 20:32:55 +0800

    dqfasaa

--- resources/views/livewire/weekly-cal.blade.php
+++ resources/views/livewire/weekly-cal.blade.php
@@ -54,14 +54,12 @@
                     @php
                         $currentDate = \Carbon\Carbon::parse($date->start_date);
                         $endDate = \Carbon\Carbon::parse($date->end_date);
-                        $need = '';
                     @endphp
 
                     @while ($currentDate->lte($endDate))
                         <th scope="col" class="px-4 py-3">
                             <button class="flex items-center gap-1">
                                 {{ $currentDate->format('M d, Y') }}
-                                {{ $need }}
                                 <svg xmlns="http://www.w3.org/2000/svg" width="11" height="11" fill="currentColor" class="bi bi-arrow-down-up" viewBox="0 0 16 16">
                                     <path fill-rule="evenodd" d="M11.5 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L11 2.707V14.5a.5.5 0 0 0 .5.5zm-7-14a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L4 13.293V1.5a.5.5 0 0 1 .5-.5z"/>
                                 </svg>
@@ -70,7 +68,6 @@
 
                         @php
                             $currentDate->addDay();
-                            $need = $currentDate->format('Y-m-d');
                         @endphp
                     @endwhile
                     <th scope="col" class="px-4 py-3">
@@ -91,26 +88,29 @@
                         @php
                             $currentDate = \Carbon\Carbon::parse($date->start_date);
                             $endDate = \Carbon\Carbon::parse($date->end_date);
+                            $need = '';
                         @endphp
 
-                        {{--@while ($currentDate->lte($endDate))
+                        @while ($currentDate->lte($endDate))
                             @foreach ($outputs as $output)
                                 @if ($output->name == $emp->name)
-                                    @if ($emp->id == 1)
-                                        i
-                                        @break
+                                    @if ($output->date == $need)
+                                        <td class="px-4 py-3">
+                                            {{ $output->output }}
+                                        </td>
+                                    @else
+                                        <td class="px-4 py-3">
+                                            0
+                                        </td>
                                     @endif
-                                @else
-                                    <td class="px-4 py-3">
-                                        0
-                                    </td>
                                 @endif
                             @endforeach
 
                             @php
                                 $currentDate->addDay();
+                                $need = $currentDate->format('Y-m-d');
                             @endphp
-                        @endwhile--}}
+                        @endwhile
                         
                         <td class="px-4 py-3 flex gap-2 items-center justify-end">
                             <a class="p-2 bg-blue-500">
