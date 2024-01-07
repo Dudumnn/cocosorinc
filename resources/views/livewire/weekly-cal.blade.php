627452f2c22a954048b20ed3ecfcd747d82a6a7d
Dudumnn <=>
2024-01-07 18:58:14 +0800

    fgyhj

--- resources/views/livewire/weekly-cal.blade.php
+++ resources/views/livewire/weekly-cal.blade.php
@@ -51,47 +51,48 @@
                             </svg>
                         </button>
                     </th>
-                    <th scope="col" class="px-4 py-3">
-                        <button class="flex items-center gap-1">
-                            @php
-                                $start = \Carbon\Carbon::parse($sched->start_date);
-                                $formattedDate = $start->format('M d, Y');
-                                echo $formattedDate;
-                            @endphp
-                            <svg xmlns="http://www.w3.org/2000/svg" width="11" height="11" fill="currentColor" class="bi bi-arrow-down-up" viewBox="0 0 16 16">
-                                <path fill-rule="evenodd" d="M11.5 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L11 2.707V14.5a.5.5 0 0 0 .5.5zm-7-14a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L4 13.293V1.5a.5.5 0 0 1 .5-.5z"/>
-                            </svg>
-                        </button>
-                    </th>
-                    {{-- The date giver --}}
-                    @php
-                        $currentDate = \Carbon\Carbon::parse($sched->start_date);
-                        $endDate = \Carbon\Carbon::parse($sched->end_date);
-                    @endphp
-
-                    @while ($currentDate->lte($endDate))
+                    @foreach ($scheds as $sched)
                         <th scope="col" class="px-4 py-3">
-                            {{ $currentDate->format('Y-m-d') }}
+                            <button class="flex items-center gap-1">
+                                @php
+                                    $start = \Carbon\Carbon::parse($sched->start_date);
+                                    $formattedDate = $start->format('M d, Y');
+                                    echo $formattedDate;
+                                @endphp
+                                <svg xmlns="http://www.w3.org/2000/svg" width="11" height="11" fill="currentColor" class="bi bi-arrow-down-up" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M11.5 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L11 2.707V14.5a.5.5 0 0 0 .5.5zm-7-14a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L4 13.293V1.5a.5.5 0 0 1 .5-.5z"/>
+                                </svg>
+                            </button>
                         </th>
-
+                        {{-- The date giver --}}
                         @php
-                            $currentDate->addDay();
+                            $currentDate = \Carbon\Carbon::parse($sched->start_date);
+                            $endDate = \Carbon\Carbon::parse($sched->end_date);
                         @endphp
-                    @endwhile
 
+                        @while ($currentDate->lte($endDate))
+                            <th scope="col" class="px-4 py-3">
+                                {{ $currentDate->format('Y-m-d') }}
+                            </th>
 
-                    <th scope="col" class="px-4 py-3">
-                        <button class="flex items-center gap-1">
                             @php
-                                $start = \Carbon\Carbon::parse($sched->end_date);
-                                $formattedDate = $start->format('M d, Y');
-                                echo $formattedDate;
+                                $currentDate->addDay();
                             @endphp
-                            <svg xmlns="http://www.w3.org/2000/svg" width="11" height="11" fill="currentColor" class="bi bi-arrow-down-up" viewBox="0 0 16 16">
-                                <path fill-rule="evenodd" d="M11.5 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L11 2.707V14.5a.5.5 0 0 0 .5.5zm-7-14a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L4 13.293V1.5a.5.5 0 0 1 .5-.5z"/>
-                            </svg>
-                        </button>
-                    </th>
+                        @endwhile
+                        <th scope="col" class="px-4 py-3">
+                            <button class="flex items-center gap-1">
+                                @php
+                                    $start = \Carbon\Carbon::parse($sched->end_date);
+                                    $formattedDate = $start->format('M d, Y');
+                                    echo $formattedDate;
+                                @endphp
+                                <svg xmlns="http://www.w3.org/2000/svg" width="11" height="11" fill="currentColor" class="bi bi-arrow-down-up" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M11.5 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L11 2.707V14.5a.5.5 0 0 0 .5.5zm-7-14a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L4 13.293V1.5a.5.5 0 0 1 .5-.5z"/>
+                                </svg>
+                            </button>
+                        </th>
+                    @endforeach
+                    
                     <th scope="col" class="px-4 py-3">
                         <span class="sr-only">Actions</span>
                     </th>
