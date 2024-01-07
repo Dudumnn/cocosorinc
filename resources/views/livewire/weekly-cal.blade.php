d942967eae090d202bb73fe5dc126e2caeea342b
Dudumnn <=>
2024-01-07 18:55:14 +0800

    poiuhbnkjj

--- resources/views/livewire/weekly-cal.blade.php
+++ resources/views/livewire/weekly-cal.blade.php
@@ -53,23 +53,40 @@
                     </th>
                     <th scope="col" class="px-4 py-3">
                         <button class="flex items-center gap-1">
-                            Date
+                            @php
+                                $start = \Carbon\Carbon::parse($sched->start_date);
+                                $formattedDate = $start->format('M d, Y');
+                                echo $formattedDate;
+                            @endphp
                             <svg xmlns="http://www.w3.org/2000/svg" width="11" height="11" fill="currentColor" class="bi bi-arrow-down-up" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M11.5 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L11 2.707V14.5a.5.5 0 0 0 .5.5zm-7-14a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L4 13.293V1.5a.5.5 0 0 1 .5-.5z"/>
                             </svg>
                         </button>
                     </th>
+                    {{-- The date giver --}}
+                    @php
+                        $currentDate = \Carbon\Carbon::parse($sched->start_date);
+                        $endDate = \Carbon\Carbon::parse($sched->end_date);
+                    @endphp
+
+                    @while ($currentDate->lte($endDate))
+                        <th scope="col" class="px-4 py-3">
+                            {{ $currentDate->format('Y-m-d') }}
+                        </th>
+
+                        @php
+                            $currentDate->addDay();
+                        @endphp
+                    @endwhile
+
+
                     <th scope="col" class="px-4 py-3">
                         <button class="flex items-center gap-1">
-                            Shift
-                            <svg xmlns="http://www.w3.org/2000/svg" width="11" height="11" fill="currentColor" class="bi bi-arrow-down-up" viewBox="0 0 16 16">
-                                <path fill-rule="evenodd" d="M11.5 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L11 2.707V14.5a.5.5 0 0 0 .5.5zm-7-14a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L4 13.293V1.5a.5.5 0 0 1 .5-.5z"/>
-                            </svg>
-                        </button>
-                    </th>
-                    <th scope="col" class="px-4 py-3">
-                        <button class="flex items-center gap-1">
-                            Output
+                            @php
+                                $start = \Carbon\Carbon::parse($sched->end_date);
+                                $formattedDate = $start->format('M d, Y');
+                                echo $formattedDate;
+                            @endphp
                             <svg xmlns="http://www.w3.org/2000/svg" width="11" height="11" fill="currentColor" class="bi bi-arrow-down-up" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M11.5 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L11 2.707V14.5a.5.5 0 0 0 .5.5zm-7-14a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L4 13.293V1.5a.5.5 0 0 1 .5-.5z"/>
                             </svg>
@@ -81,7 +98,7 @@
                 </tr>
             </thead>
             <tbody>
-                @forelse ($scheds as $sched)
+                {{--@forelse ($scheds as $sched)
                     <tr class="border-b">
                         <th class="px-4 py-3 font-medium text-gray-900 whitespace-nowrap">
                             {{ $sched->id }}
@@ -137,7 +154,7 @@
                             No items found.
                         </td>
                     </tr>
-                @endforelse
+                @endforelse--}}
             </tbody>
         </table>
     </div>
