d8b367a22f193106fafa01f177eeed42f3e98feb
Dudumnn <=>
2024-01-10 22:10:08 +0800

    dsij

--- resources/views/livewire/weekly-cal.blade.php
+++ resources/views/livewire/weekly-cal.blade.php
@@ -319,7 +319,7 @@
                         </svg>
                     </div>
                     <input 
-                        wire:model.live.debounce.300ms = "search"
+                        wire:model.live.debounce.100ms = "search"
                         type="text"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2 "
                         placeholder="Search" required="">
